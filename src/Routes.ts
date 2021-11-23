4e2422e2307150d8670d35f672f40b16bbdec9f3
Wade Harkins <vdtdev@gmail.com>
2021-11-23 16:03:05 -0500

    update babel config to support property decorators in lit

---
+++ src/Routes.ts
@@ -0,0 +1,53 @@
+/**
+ * Individual route for a navigation item
+ */
+export interface Route {
+  label: string;
+  path: string;
+  group?: string;
+  component?: unknown;
+  framework?: string;
+}
+
+/** Collection of Routes */
+export type Routes = Route[];
+
+// #region [Route Tree Parsing]
+export enum RouteTreeItemType {
+  GROUP = "group",
+  ROUTE = "route",
+}
+
+export interface RouteTreeItem {
+  type: RouteTreeItemType;
+  // index of route in Routes if this is a route; default to -1 for group?
+  index: number;
+  // label (for group)
+  label?: string;
+  children?: RouteTreeItem[];
+}
+
+export type RouteTree = RouteTreeItem[];
+
+export const resolveRouteStructure = (routes: Routes): RouteTree => {
+  // TODO: Have this build out a route tree e.g.:
+  /*
+    Input: [
+        {label: 'Intro', path: 'intro'},
+        {label: 'Push Button', group: 'Inputs/Buttons', path: 'push-button'},
+        {label: 'Reset Button', group: 'Inputs/Buttons', path: 'reset-button'}
+    ]
+
+    Output: [
+        { type: 'route', index: 0 },
+        { type: 'group', label: 'Inputs', children: [
+            { type: 'group', label: 'Buttons', children: [
+                { type: 'route': index: 1 },
+                { type: 'route': index: 2 }
+            ]}
+        ]}
+    ]
+    */
+  return [];
+};
+// #endregion
