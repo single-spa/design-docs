09da80044548b9932c804239b3080c332461fe1c
joeldenning <joeldenning@gmail.com>
2021-12-20 15:48:39 -0700

    Comment

--- src/Globals.ts
+++ src/Globals.ts
@@ -1,1 +1,4 @@
+// This base url is used to load any static images, fonts, etc.
+// For in-browser usage of design-docs, this works well.
+// Webpack usage of design-docs is something I'll think more about.
 window.designDocsBaseUrl = new URL("..", import.meta.url).href;
