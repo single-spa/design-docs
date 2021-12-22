3e25aa5daed724f91b5df63bd1c9af28f0ce8adc
Wade Harkins <vdtdev@gmail.com>
2021-12-22 13:56:40 -0500

    Add initial route tree resolution logic and utilize for nav tree property input

--- src/DesignDocComponents.ts
+++ src/DesignDocComponents.ts
@@ -2,3 +2,4 @@ import "./Globals";
 import "./Root";
 import "./SideBar";
 import "./SideNav";
+export { resolveRouteStructure } from "./Routes";
--- src/Routes.ts
+++ src/Routes.ts
@@ -11,6 +11,8 @@ export enum RouteFramework {
   HTML = "html",
 }
 
+export const RouteGroupDelimiter = "/";
+
 /**
  * Individual route for a navigation item
  */
@@ -42,16 +44,18 @@ export interface RouteTreeItem {
 
 export type RouteTree = RouteTreeItem[];
 
-export const resolveRouteStructure = (routes: Routes): RouteTree => {
-  // TODO: Have this build out a route tree e.g.:
-  /*
-    Input: [
-        {label: 'Intro', path: 'intro'},
+/**
+ * Constructs a tree from routing information for use by navigation component
+ * @param routes Routing information
+ * @returns Structured route navigation tree
+ * @example
+ * const input = [
+        {lbel: 'Intro', path: 'intro'},
         {label: 'Push Button', group: 'Inputs/Buttons', path: 'push-button'},
         {label: 'Reset Button', group: 'Inputs/Buttons', path: 'reset-button'}
-    ]
-
-    Output: [
+    ];
+ 
+    const output = [
         { type: 'route', index: 0 },
         { type: 'group', label: 'Inputs', children: [
             { type: 'group', label: 'Buttons', children: [
@@ -59,9 +63,81 @@ export const resolveRouteStructure = (routes: Routes): RouteTree => {
                 { type: 'route': index: 2 }
             ]}
         ]}
-    ]
-   
-    */
-  return [];
+    ];
+ */
+export const resolveRouteStructure = (routes: Routes): RouteTree => {
+  const tree: RouteTree = [];
+  const groupRefMap: Record<string, RouteTreeItem> = {};
+
+  const traceGroupRef = (groupName: string): RouteTreeItem[] => {
+    const groupPath = groupName.split(RouteGroupDelimiter);
+    const refs: RouteTreeItem[] = [];
+    for (let i = 0; i < groupPath.length; i++) {
+      const ref =
+        groupRefMap[groupPath.slice(0, i + 1).join(RouteGroupDelimiter)];
+      if (ref) {
+        refs.push(ref);
+      } else {
+        break;
+      }
+    }
+    return refs;
+  };
+
+  const buildGroups = (groupName: string): RouteTreeItem => {
+    const refs = traceGroupRef(groupName);
+    const groupPath = groupName.split(RouteGroupDelimiter).slice(refs.length);
+    let group: RouteTreeItem = {
+      index: -1,
+      type: RouteTreeItemType.GROUP,
+      label: "",
+      children: [],
+    };
+    let parent = refs[refs.length - 1];
+    const groupPrefix = refs.length
+      ? `${groupName
+          .split(RouteGroupDelimiter)
+          .slice(0, refs.length)
+          .join(RouteGroupDelimiter)}${RouteGroupDelimiter}`
+      : "";
+    for (let i = 0; i < groupPath.length; i++) {
+      group = {
+        ...group,
+        label: groupPath[i],
+      };
+      const name = groupPrefix.concat(
+        groupPath.slice(0, i + 1).join(RouteGroupDelimiter)
+      );
+      groupRefMap[name] = group;
+      if (parent) {
+        parent.children?.push(group);
+      } else {
+        tree.push(group);
+      }
+      parent = group;
+    }
+    return group;
+  };
+
+  for (let index = 0; index < routes.length; index++) {
+    const route = routes[index];
+    if (route.group) {
+      let groupRef = groupRefMap[route.group];
+      if (!groupRef) {
+        groupRef = buildGroups(route.group);
+      }
+      groupRef.children?.push({
+        index,
+        type: RouteTreeItemType.ROUTE,
+      });
+    } else {
+      tree.push({
+        index,
+        type: RouteTreeItemType.ROUTE,
+      });
+    }
+  }
+
+  return tree;
 };
 // #endregion
